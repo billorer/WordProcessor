bd1f20a3d8740d22d47cf765ef087a8fb580628f
zoran.suto <zoran@suto.ro>
2017-08-21 16:01:09 +0300

    The answers class handles the answers, creates them and after them they are exported to an XML file.

---
+++ WordDocProcessor/Answers.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDocProcessor
+{
+    class Answers
+    {
+        private int seqParagraph = 1;
+        private int seqListDecimal = 2;
+        private int seqCircleList = 3;
+        private int seqNotesList = 4;
+        private int seqImages = 5;
+        private int seqTables = 6;
+
+        private List<string> listParagraph;
+
+        private List<string> listDecimal; // nemartana ezeket matrixolni, ha esetlegesen tobb felsorolas van egy paragrafus reszben
+        private List<string> listCircle;
+
+        private List<string> listNotes;
+
+        private List<string> listTables;
+
+        /// <summary>
+        /// The Sequence of the elements
+        /// 1. -> paragraph
+        /// 2. -> decimalList
+        /// 3. -> circleList
+        /// 4. -> notesList
+        /// 5. -> images
+        /// 6. -> tables
+        /// </summary>
+        private List<int> sequence;
+
+
+        public int SeqParagraph
+        {
+            get { return seqParagraph; }
+        }
+        public int SeqCircleList
+        {
+            get { return seqCircleList; }
+        }
+        public int SeqDecimalList
+        {
+            get { return seqListDecimal; }
+        }
+        public int SeqNotesList
+        {
+            get { return seqNotesList; }
+        }
+        public int SeqImage
+        {
+            get { return seqImages; }
+        }
+        public int SeqTables
+        {
+            get { return seqTables; }
+        }
+
+        public List<string> ListParagraph
+        {
+            get { return listParagraph; }
+            set { listParagraph = value; }
+        }
+
+        public string SetListParagraphElement
+        {
+            set
+            {
+                listParagraph.Add(value);
+            }
+        }
+
+        public List<string> ListDecimal
+        {
+            get { return listDecimal; }
+            set { listDecimal = value; }
+        }
+
+        public string SetListDecimalElement
+        {
+            set {
+                listDecimal.Add(value);    
+            }
+        }
+
+        public List<string> ListCircle
+        {
+            get { return listCircle; }
+            set { listCircle = value; }
+        }
+
+        public string SetListCircleElement
+        {
+            set
+            {
+                listCircle.Add(value);
+            }
+        }
+
+        public List<string> ListNotes
+        {
+            get { return listNotes; }
+            set { listNotes = value; }
+        }
+
+        public string SetListNotesElement
+        {
+            set
+            {
+                listNotes.Add(value);
+            }
+        }
+
+        public List<string> ListTables
+        {
+            get { return listTables; }
+            set { listTables = value; }
+        }
+
+        public string SetListTablesElement
+        {
+            set
+            {
+                listTables.Add(value);
+            }
+        }
+
+        public List<int> Sequence
+        {
+            get { return sequence; }
+            set { sequence = value; }
+        }
+
+        public int SetSequenceElement
+        {
+            set
+            {
+                sequence.Add(value);
+            }
+        }
+
+        public Answers()
+        {
+            listParagraph = new List<string>();    
+            listDecimal = new List<string>();
+            listCircle = new List<string>();
+            listNotes = new List<string>();
+            listTables = new List<string>();
+
+            sequence = new List<int>();
+        }
+    }
+}
--- WordDocProcessor/Program.cs
+++ WordDocProcessor/Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Xml.Linq;
+
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace WordDocProcessor
 {
     class Program
     {
         private static Microsoft.Office.Interop.Word.Application word;
-        private static Microsoft.Office.Interop.Word.Document docs;
+        private static Document docs;
 
         /// <summary>
         /// The list in which we keep the questions
@@ -19,41 +25,111 @@ namespace WordDocProcessor
         /// <summary>
         /// The list in which we keep the answers
         /// </summary>
-        private static List<string> answers;
+        private static List<Answers> answersList;
+
+        private static Answers currentAnswer;
+
+        private static List<string> docTablesList;
+
+        private static int imageNumber = 1;
+        private static int tableNumber = 0;
 
         /// <summary>
         /// This function creates a WORD DOCUMENT object and returns it.
         /// It Opens the adequate word document.
         /// </summary>
         /// <returns></returns>
-        private static Microsoft.Office.Interop.Word.Document GetDocument(){
+        private static Document GetDocument(){
             word = new Microsoft.Office.Interop.Word.Application();
             object miss = System.Reflection.Missing.Value;
-            object path = @"c:\Users\zoran.suto\Documents\OMG.docx";
+            object path = @"c:\Users\zoran.suto\Documents\OMG3.docx";
             object readOnly = true;
             docs = word.Documents.Open(ref path, ref miss, ref readOnly, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss, ref miss);
             return docs;  
         }
-
+        private static string stripCellText(string text)
+        {
+            return text.Replace("\r", "");
+        }
         static void Main(string[] args)
         {
             // Initializing the list variables
             questions = new List<string>();
-            answers = new List<string>();
+            answersList = new List<Answers>();
+            currentAnswer = new Answers();
+            docTablesList = new List<string>();
 
-            Microsoft.Office.Interop.Word.Document docs = GetDocument();
+            Document docs = GetDocument();
 
-            string answer = "";
             string heading2 = "";
             string heading3 = "";
             string heading4 = "";
             string previousParentHeading = "";
 
+            bool foundFirstHeading = false;
+            bool signTable = false;          
+
+            ExtractImagesFromDocIntoWordDocImages();
+
+            
+
+            foreach (Table tb in docs.Tables)
+            {
+                string table = "<table>";
+                for (int row = 1; row <= tb.Rows.Count; row++)
+                {
+                    table += "<tr>";
+                    for (int column = 1; column <= tb.Columns.Count; column++)
+                    {
+                        if(column == 1)
+                        {
+                            table += "<th>";
+                        }
+                        else
+                        {
+                            table += "<td>";
+                        }
+
+                        try
+                        {
+                            var text = stripCellText(tb.Cell(row, column).Range.Text);
+                            text = RemoveNewLineFromString(text);
+                            text = CleanInvalidXmlChars(text);
+                            table += text;
+                            //Console.WriteLine("Row: " + row + " Column: " + column + " Text: " + text);
+                        }
+                        catch (System.Runtime.InteropServices.COMException)
+                        {
+
+                        }
+
+                        if (column == 1)
+                        {
+                            table += "</th>";
+                        }
+                        else
+                        {
+                            table += "</td>";
+                        }
+                    }
+                    table += "</tr>";
+                }
+                table += "</table>";
+                docTablesList.Add(table);
+            }
+
+
+
+            ExportDataToXMLFile("tables","table", @"c:\tables.xml", docTablesList);
+
+
+            //docTablesList = XDocument.Load(@"c:\tables.xml").Root.Elements("table")
+            //               .Select(element => element.Value)
+            //               .ToList();
+
             for (int i = 0; i < docs.Paragraphs.Count; i++)
             {
-
-
-                Microsoft.Office.Interop.Word.Style style = docs.Paragraphs[i + 1].get_Style() as Microsoft.Office.Interop.Word.Style;
+                Style style = docs.Paragraphs[i + 1].get_Style() as Style;
                 string styleName = style.NameLocal;
                 string currentText = docs.Paragraphs[i + 1].Range.Text.ToString();
 
@@ -61,81 +137,166 @@ namespace WordDocProcessor
                 currentText = RemoveNewLineFromString(currentText);
                 currentText = CleanInvalidXmlChars(currentText);
 
-                //if (i % 100 == 0)
-                //{
-                //    Console.WriteLine(currentText);
-                //}
+                // Just to get minimal information about the program when it runs
+                if (i % 100 == 0)
+                {
+                    Console.WriteLine(currentText);
+                }
+                //Console.WriteLine("Style: " + styleName + " Text: " + currentText);
 
-                if (styleName == "Heading 2")
+                // From now on we can start adding questions and their answers
+                if(styleName == "Heading 2")
                 {
-                    heading2 = currentText;
-                    previousParentHeading = heading2;
-                    questions.Add(heading2);
-                    if (questions.Count != 0 && answer != "")
-                    {                       
-                        answers.Add(answer);
-                        answer = "";
-                    }
+                    foundFirstHeading = true;
                 }
-                else if (styleName == "Heading 3")
+
+                if (foundFirstHeading)
                 {
-                    heading3 = heading2 + " " + currentText;
-                    previousParentHeading = heading3;
-                    
-                    if (questions.Count != 0 && answer != "")
+                    if (styleName == "Heading 2")
                     {
+                        heading2 = currentText;
+                        previousParentHeading = heading2;
+                        questions.Add(heading2);
+
+                        AddAnswerAndCreateNewOne();
+
+                    }
+                    else if (styleName == "Heading 3")
+                    {
+                        heading3 = heading2 + " " + currentText;
+                        previousParentHeading = heading3;
+
                         questions.Add(heading3);
-                        answers.Add(answer);
-                        answer = "";
+                        AddAnswerAndCreateNewOne();
+
                     }
-                }
-                else if (styleName == "Heading 4")
-                {
-                    heading4 = heading3 + " " + currentText;
-                    previousParentHeading = heading4;
-                    
-                    if (questions.Count != 0 && answer != "")
+                    else if (styleName == "Heading 4")
                     {
+                        heading4 = heading3 + " " + currentText;
+                        previousParentHeading = heading4;
+
                         questions.Add(heading4);
-                        answers.Add(answer);
-                        answer = "";
+                        AddAnswerAndCreateNewOne();
+
                     }
-                }else if(styleName == "Titel:Label")
-                {
-                    
-                    if (questions.Count != 0 && answer != "")
+                    else if (styleName == "Titel:Label")
                     {
                         questions.Add(previousParentHeading + " " + currentText);
-                        answers.Add(answer);
-                        answer = "";
+                        AddAnswerAndCreateNewOne();
                     }
-                }
-                else if (!currentText.Contains("Figure") || currentText != " / ") //the currentText should contain only the answer section
-                {
-                    if (questions.Count != 0)
+                    else if (styleName == "Liste:Punkt")
                     {
-                        answer += currentText + " ";
+                        currentAnswer.SetListCircleElement = currentText;
+                        currentAnswer.SetSequenceElement = currentAnswer.SeqCircleList;
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Style: " + styleName + " Text: " + currentText);
-                }
-                if (i == docs.Paragraphs.Count - 1) //the last questions answer
-                {
-                    answers.Add(answer);
-                    answer = "";
+                    else if (styleName == "Liste:Nummer")
+                    {
+                        currentAnswer.SetListDecimalElement = currentText;
+                        currentAnswer.SetSequenceElement = currentAnswer.SeqDecimalList;
+                    }
+                    else if (styleName == ":imp")
+                    {
+                        currentAnswer.SetListNotesElement = currentText;
+                        currentAnswer.SetSequenceElement = currentAnswer.SeqNotesList;
+                    }
+                    else if (styleName.Contains("Tab:Kopf"))
+                    {
+                        //currentAnswer.SetListNotesElement = currentText;
+                        if (!signTable)
+                        {
+                            currentAnswer.SetSequenceElement = currentAnswer.SeqTables;
+                            currentAnswer.SetListTablesElement = docTablesList[tableNumber++];
+                        }
+                        
+                        signTable = true;
+                    }
+                    else if (styleName.Contains("Tab:Absatz"))
+                    {
+                        //currentAnswer.SetListNotesElement = currentText;
+                        //currentAnswer.SetSequenceElement = currentAnswer.SeqNotesList;
+                        signTable = false;
+                    }
+                    else if (currentText == "/")
+                    {
+                        currentAnswer.SetListParagraphElement = currentText.Replace("/", "img_" + imageNumber.ToString());
+                        imageNumber++;
+                        currentAnswer.SetSequenceElement = currentAnswer.SeqImage;
+                    }
+                    else if (currentText != "")
+                    {
+                        currentAnswer.SetListParagraphElement = currentText;
+                        currentAnswer.SetSequenceElement = currentAnswer.SeqParagraph;
+                    }
+
+                    if (i == docs.Paragraphs.Count - 1) //the last questions answer
+                    {
+                        AddAnswerAndCreateNewOne();
+                    }
                 }
             }
 
             docs.Close();
             word.Quit();
 
-            ExportDataToXMLFile("answers", "answer", @"c:\answers.xml", answers);
+            ExportDataToXMLFile("answers", "answer", @"c:\answers.xml", answersList);
             ExportDataToXMLFile("questions", "question", @"c:\questions.xml", questions);
 
             Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        private static void AddAnswerAndCreateNewOne()
+        {
+            if(currentAnswer.Sequence.Count != 0)
+            {
+                answersList.Add(currentAnswer);
+                currentAnswer = new Answers();
+            }
+        }
+
+        private static void ExtractImagesFromDocIntoWordDocImages()
+        {
+            for (var index = 1; index <= word.ActiveDocument.InlineShapes.Count; index++)
+            {
+                var inlineShapeId = index;
+
+                // parameterized thread start
+                var thread = new Thread(() => SaveInlineShapeToFile(inlineShapeId));
+
+                // STA is needed in order to access the clipboard
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+                thread.Join();
+            }
+        }
+
+        protected static void SaveInlineShapeToFile(int inlineShapeId)
+        {
+            // Get the shape, select, and copy it to the clipboard
+            var inlineShape = docs.InlineShapes[inlineShapeId];
+            inlineShape.Select();
+            docs.ActiveWindow.Selection.CopyAsPicture();
+
+            // Check data is in the clipboard
+            if (Clipboard.GetDataObject() != null)
+            {
+                var data = Clipboard.GetDataObject();
+
+                // Check if the data conforms to a bitmap format
+                if (data != null && data.GetDataPresent(DataFormats.Bitmap))
+                {
+                    // Fetch the image and convert it to a Bitmap
+                    Image image = (Image)data.GetData(DataFormats.Bitmap, true);
+
+                    if (image != null)
+                    {
+                        var currentBitmap = new Bitmap(image);
+                        System.IO.Directory.CreateDirectory(@"c:\WordDocImages\");
+                        // Save the bitmap to a file
+                        currentBitmap.Save(@"c:\WordDocImages\" + String.Format("img_{0}.png", inlineShapeId));
+                    }
+                }
+}
         }
 
         /// <summary>
@@ -162,6 +323,31 @@ namespace WordDocProcessor
         {
             new XElement(rootElementName, data.Select(i => new XElement(childElementName, i)))
                 .Save(filePath, SaveOptions.OmitDuplicateNamespaces);
+        }
+
+        private static void ExportDataToXMLFile(string rootElementName, string childElementName, string filePath, List<Answers> data)
+        {
+            new XElement("answers",
+                data.Select(answerElement => new XElement("answer",
+                    new XElement("paragraphList", answerElement.ListParagraph.Select(paragraphList =>
+                        new XElement("paragraphListElement", paragraphList
+                    ))),
+                    new XElement("circleList", answerElement.ListCircle.Select(circleList => 
+                        new XElement("circleListElement", circleList
+                    ))),
+                    new XElement("decimalList", answerElement.ListDecimal.Select(decimalList =>
+                        new XElement("decimalListElement", decimalList
+                    ))),
+                    new XElement("notesList", answerElement.ListNotes.Select(noteList =>
+                        new XElement("noteListElement", noteList
+                    ))),
+                    new XElement("tableList", answerElement.ListTables.Select(tableList =>
+                        new XElement("table", tableList
+                    ))),
+                    new XElement("sequence", answerElement.Sequence.Select(sequence =>
+                        new XElement("sequenceElement", sequence
+                    )))
+                ))).Save(filePath, SaveOptions.OmitDuplicateNamespaces);
         }
 
         /// <summary>
